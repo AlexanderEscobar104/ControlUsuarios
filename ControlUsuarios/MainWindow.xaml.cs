bd21a51ff5598d9a3317ed31cfbbf564574a3b50
Pisco Company Tics <80163394+piscotics@users.noreply.github.com>
2023-04-12 13:08:15 -0500

    Agregar archivos de proyecto.

---
+++ ControlUsuarios/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlUsuarios
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //lista todos los usuarios
+            listDatos();
+
+        }
+        //lista todos los usuarios
+        private void listDatos()
+        {
+            try
+            {
+                SqlConnection thisConnection = new SqlConnection(@"Server=DESKTOP-DOLID7H\MSSQLSERVER01;Database=DatosPersonales;Trusted_Connection=True;Encrypt=False");
+                thisConnection.Open();
+                //realiza la consulta de los datos
+                string Get_Data = "SELECT Id_Usuario #, Nombre, Apellido  FROM Usuarios";
+
+                SqlCommand cmd = thisConnection.CreateCommand();
+                cmd.CommandText = Get_Data;
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable("Usuarios");
+                sda.Fill(dt);
+               
+                DGDatosPersonales.ItemsSource = dt.DefaultView;
+
+               
+               
+            }
+            catch
+            {
+                MessageBox.Show("error al conectar a la bd ");
+            }
+        }
+        
+    }
+}
